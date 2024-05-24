d21e5a88a03ffdc2b49257f42739a9bf27535164
Allan Reyes <allanreyes@microsoft.com>
2024-05-24 12:17:52 -0400

    Add password rotate function

---
+++ src/rotate/run.ps1
@@ -0,0 +1,20 @@
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+$sitecode = $Request.Body.sitecode
+
+# Generate a 9 character password using only letters and numbers
+$password = ([char[]]([char]97..[char]122) + ([char[]]([char]65..[char]90)) + 0..9 | 
+    Sort-Object { Get-Random })[0..9] -join '' | 
+    ForEach-Object { $_.ToUpper() }
+
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+
+#Connect to Azure Keyvault and create a new version of the secret
+Set-AzKeyVaultSecret -VaultName $env:KeyVaultName -Name $sitecode -SecretValue $securePassword
+
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body       = $password
+    })
