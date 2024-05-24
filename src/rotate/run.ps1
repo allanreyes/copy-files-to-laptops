402aa4eb2e21149ae2a7a29e1ebfc1ef72f72813
Allan Reyes <allanreyes@microsoft.com>
2024-05-24 16:59:07 -0400

    Remove letter o and 0 from the password

--- src/rotate/run.ps1
+++ src/rotate/run.ps1
@@ -3,9 +3,10 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 
 $sitecode = $Request.Body.sitecode
-
+# [char]111 = o
+# [char]79 = O
 # Generate a 9 character password using only letters and numbers
-$password = ([char[]]([char]97..[char]122) + ([char[]]([char]65..[char]90)) + 0..9 | 
+$password = ([char[]] ([char]97..[char]110) + ([char]112..[char]122) + ([char]65..[char]78) +([char]80..[char]90) + 1..9 | 
     Sort-Object { Get-Random })[0..9] -join '' | 
     ForEach-Object { $_.ToUpper() }
 
